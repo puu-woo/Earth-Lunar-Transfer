38cc7ee0a453950c2d717d226f11a849d63f5b36
eunbyeeol <12200625@inha.edu>
2023-05-16 18:43:55 +0900

    update

---
+++ 2Dstudy/2Dstudy_eunbyeol/TransOrb.m
@@ -0,0 +1,27 @@
+function [orb_t,T] = TransOrb(r0,v_init,lunar_posATinj,lunar_SOI,dt)
+
+[orb_t,min_distance,T] = EorbitRK4(dt,[r0,v_init],lunar_posATinj,lunar_SOI);
+
+tor = 0.01;
+addv = [0,-0.01,0];
+pre_level = '+';
+while true
+    if min_distance > tor
+        if pre_level == '-'
+            addv = addv/10;
+        end
+        pre_level = '+';
+        v_init = v_init+addv;
+        [orb_t,min_distance,T] = EorbitRK4(dt,[r0,v_init],lunar_posATinj,lunar_SOI);
+    elseif min_distance < -tor
+        if pre_level == '+'
+            addv = addv/10;
+        end
+        pre_level = '-';
+
+        v_init = v_init-addv;
+        [orb_t,min_distance,T] = EorbitRK4(dt,[r0,v_init],lunar_posATinj,lunar_SOI);
+    else
+        break;
+    end
+end
